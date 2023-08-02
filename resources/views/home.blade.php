fad53fae38080d0386d8fca6ce9a6d7023fd75bd
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-02 13:39:13 +0200

    print data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>ciao</h1>
+    <h1>C.V</h1>
+    <h3>Nome : {{$name}}</h3>
+    <h3>Lavoro : {{$lavoro}}</h3>
+    <h3>Mansione : {{$descrizione}}</h3>
 </body>
 </html>
