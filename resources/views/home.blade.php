2915a8ffbffa2346527ebf0bd73b40646a7e4b84
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-03 16:22:25 +0200

    add date

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>C.V</h1>
+    <h2>dati:</h2>
     <h3>Nome : {{$name}}</h3>
     <h3>Lavoro : {{$lavoro}}</h3>
     <h3>Mansione : {{$descrizione}}</h3>
 
-    <a href="{{ route('info') }}" >vedi foto di {{$name}}</a>
+    <a href="{{ route('info') }}">vedi foto di {{$name}}</a>
 </body>
+
 </html>
